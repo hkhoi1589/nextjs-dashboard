9c240074703047c1b5611ca6c3035c6ed92cacc2
hkhoi1589 <hkhoi1589@gmail.com>
2024-04-23 16:07:48 +0700

    next is chap 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,73 +9,96 @@ import { z } from 'zod';
 
 const FormSchema = z.object({
 	id: z.string(),
-	customerId: z.string(),
-	amount: z.coerce.number(),
-	status: z.enum(['pending', 'paid']),
+	customerId: z.string({
+		invalid_type_error: 'Please select a customer.',
+	}),
+	amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than $0.' }),
+	status: z.enum(['pending', 'paid'], {
+		invalid_type_error: 'Please select an invoice status.',
+	}),
 	date: z.string(),
 });
+
+// contains the state passed from the useFormState hook
+export type State = {
+	errors?: {
+		customerId?: string[];
+		amount?: string[];
+		status?: string[];
+	};
+	message?: string | null;
+};
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-	// Type validation and coercion
-	const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+	// Validate form using Zod
+	const validatedFields = CreateInvoice.safeParse({
 		customerId: formData.get('customerId'),
 		amount: formData.get('amount'),
 		status: formData.get('status'),
 	});
 
-	// Let's convert the amount into cents
+	// If form validation fails, return errors early. Otherwise, continue.
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: 'Missing Fields. Failed to Create Invoice.',
+		};
+	}
+
+	// Prepare data for insertion into the database
+	const { customerId, amount, status } = validatedFields.data;
 	const amountInCents = amount * 100;
 	const date = new Date().toISOString().split('T')[0];
 
-	// insert the new invoice into your database or call api
+	// Insert data into the database
 	try {
 		await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
 	} catch (error) {
+		// If a database error occurs, return a more specific error.
 		return {
 			message: 'Database Error: Failed to Create Invoice.',
 		};
 	}
 
-	// Since you're updating the data displayed in the invoices route, you need revalidated this route to see it work
+	// Revalidate the cache for the invoices page and redirect the user.
 	revalidatePath('/dashboard/invoices');
-
-	// redirect the user to the invoice's page
 	redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-	// Extracting the data from formData.
-	// And validating the types with Zod
-	const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+	const validatedFields = UpdateInvoice.safeParse({
 		customerId: formData.get('customerId'),
 		amount: formData.get('amount'),
 		status: formData.get('status'),
 	});
 
-	// Converting the amount to cents
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: 'Missing Fields. Failed to Update Invoice.',
+		};
+	}
+
+	const { customerId, amount, status } = validatedFields.data;
 	const amountInCents = amount * 100;
 
-	// update the invoice into your database or call api
 	try {
 		await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-      `;
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
 	} catch (error) {
 		return { message: 'Database Error: Failed to Update Invoice.' };
 	}
 
-	// clear the client cache and make a new server request
 	revalidatePath('/dashboard/invoices');
-
-	// redirect the user to the invoice's page
 	redirect('/dashboard/invoices');
 }
 
